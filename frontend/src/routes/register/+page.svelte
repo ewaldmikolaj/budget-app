019474388d2abbc1e1ba92ff8e19bd276f7806a6
ewaldmikolaj <mikolajewald@gmail.com>
2025-03-25 20:56:46 +0100

    authentication page created

---
+++ frontend/src/routes/register/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    import RegisterForm from "$lib/components/RegisterForm.svelte";
+</script>
+
+<main class="h-screen flex items-center justify-center bg-emerald-500">
+    <RegisterForm />
+</main>
+
+<style lang="postcss">
+    @reference "tailwindcss";
+  </style>
