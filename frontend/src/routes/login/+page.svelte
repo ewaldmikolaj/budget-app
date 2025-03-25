49a54936d0a48a676c7ef4bf8b3c6144932140b7
ewaldmikolaj <mikolajewald@gmail.com>
2025-03-25 20:44:53 +0100

    login page created

---
+++ frontend/src/routes/login/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    import LoginForm from "$lib/components/LoginForm.svelte";
+</script>
+
+<main class="h-screen flex items-center justify-center bg-emerald-500">
+    <LoginForm />
+</main>
+
+<style lang="postcss">
+    @reference "tailwindcss";
+  </style>
