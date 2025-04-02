ac748b50ff5f49552c59327112c081c27cacb50a
ewaldmikolaj <mikolajewald@gmail.com>
2025-04-02 23:30:44 +0200

    secured path created

--- frontend/src/app.d.ts
+++ frontend/src/app.d.ts
@@ -1,12 +1,17 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
+type User = {
+	id: number;
+	email: string;
+	name: string;
+	surname: string;
+};
+
 declare global {
 	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface PageState {}
-		// interface Platform {}
+		interface Locals {
+			user: User | null;
+		}
 	}
 }
 
